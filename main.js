1eefad1ec1d753b156fa481a87fca2f24b478a56
Shazoolkids <81330320+Shazoolkids@users.noreply.github.com>
2022-05-23 22:26:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+localStorage.setItem("player1_name", player1_name);
+localStorage.setItem("player2_name", player2_name);
+
+window.location = "gamepage.html";
+}
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = " Answer Turn - " + player2_name;
+
+
+
+
